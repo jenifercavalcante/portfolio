f02ce24e96a488c1e44d57143e9447a500d14f4e
Jenifer <jenifer.lima@normatel.com.br>
2023-06-12 22:28:27 -0300

    chore(style): adjust card layout on home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,7 +5,7 @@ import "./home.scss";
 function Home() {
   return (
     <div className="home">
-      <Card title="Lorem ipsum">
+      <Card>
         <div className="container-fluid">
           <div className="row">
             <div className="col-12 col-md-6 window">
@@ -13,28 +13,15 @@ function Home() {
             </div>
 
             <div className="col-12 col-md-6 about">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
-              maximus risus quis aliquam volutpat. Curabitur vestibulum eros ut
-              elit lobortis, eu ultrices urna rutrum. Sed quam leo, vulputate
-              vitae tellus eget, mattis lobortis augue. Aliquam vulputate neque
-              eget condimentum vestibulum. Suspendisse porta massa vitae sem
-              tincidunt, vel auctor arcu ullamcorper. Integer ut diam ac nulla
-              rutrum interdum nec non erat. Nulla facilisi. Duis porttitor
-              ligula et lobortis cursus. Duis fringilla ligula vitae orci
-              tristique, egestas lobortis mi congue. Nunc eget pretium arcu.
-              Suspendisse commodo elementum feugiat. Morbi fermentum porttitor
-              est quis varius. Nunc vitae pharetra massa, a pulvinar quam.
-              Pellentesque vitae eros fermentum, eleifend nunc vitae,
-              scelerisque risus. Integer hendrerit tincidunt porta. Maecenas
-              euismod sapien id lacus tempor, ut commodo dui pharetra. Duis
-              nulla risus, dignissim eu eleifend eu, suscipit vel nunc.
-              Pellentesque tincidunt lobortis nisl, eget porta ex elementum sed.
-              Phasellus facilisis mi nec pellentesque pellentesque. Pellentesque
-              dolor erat, cursus vitae justo sed, sollicitudin auctor neque.
-              Curabitur eu dolor ullamcorper lectus iaculis fringilla. Maecenas
-              eleifend mi et enim consectetur, vulputate pellentesque nisi
-              blandit. Aliquam maximus in sem sed vestibulum. Integer eget quam
-              in ex tincidunt blandit ut in arcu.
+              <span className="subtitle">ABOUT ME</span>
+              <span className="description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                maximus risus quis aliquam volutpat. Curabitur vestibulum eros
+                ut elit lobortis, eu ultrices urna rutrum. Sed quam leo,
+                vulputate vitae tellus eget, mattis lobortis augue. Aliquam
+                vulputate neque eget condimentum vestibulum. Suspendisse porta
+                massa vitae sem tincidunt, vel auctor arcu ullamcorper.
+              </span>
             </div>
           </div>
         </div>
